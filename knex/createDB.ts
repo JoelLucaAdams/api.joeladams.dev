2f92d0adb49da0b9df6b2bf73c0cf43e0cd3ce22
JoelLucaAdams <joelucadams@gmail.com>
2022-01-10 11:12:12 +0000

    Moved knexfile to parent directory

--- knex/createDB.ts
+++ knex/createDB.ts
@@ -1,17 +1,17 @@
 import knex from 'knex';
 import log from '../log';
-import knexfile from './knexfile';
+import knexfile from '../knexfile';
 
 const knexClient = knex(knexfile.development);
 
 async function createDatabase() {
-    try {
-        console.log('Create database if it doesn\'t exist...')
-        // await knexClient.raw(`SELECT 'CREATE DATABASE \`${knexfile.development.connection.database}\`' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = \`${knexfile.development.connection.database}\`)\gexec`)
-    } catch (err) {
-        console.log(err);
-    }
-    console.log('Done')
+  try {
+    console.log("Create database if it doesn't exist...");
+    // await knexClient.raw(`SELECT 'CREATE DATABASE \`${knexfile.development.connection.database}\`' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = \`${knexfile.development.connection.database}\`)\gexec`)
+  } catch (err) {
+    console.log(err);
+  }
+  console.log('Done');
 }
 
 createDatabase();
