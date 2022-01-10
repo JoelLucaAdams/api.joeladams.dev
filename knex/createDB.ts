4587539ff9232e7de743f9b3813a62190e097be5
JoelLucaAdams <joelucadams@gmail.com>
2022-01-10 14:38:41 +0000

    Added TODO to create database

--- knex/createDB.ts
+++ knex/createDB.ts
@@ -7,6 +7,7 @@ const knexClient = knex(knexfile.development);
 async function createDatabase() {
   try {
     console.log("Create database if it doesn't exist...");
+    //TODO: Add database command
     // await knexClient.raw(`SELECT 'CREATE DATABASE \`${knexfile.development.connection.database}\`' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = \`${knexfile.development.connection.database}\`)\gexec`)
   } catch (err) {
     console.log(err);
